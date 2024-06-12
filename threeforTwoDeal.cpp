662ed18ccda0fdea5fb52a51af03cb04b0154e27
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-12 14:52:30 -0700

    Moved logic from cart into apply method

--- threeforTwoDeal.cpp
+++ threeforTwoDeal.cpp
@@ -1,21 +1,43 @@
 // threeForTwoDeal.cpp
 
 #include "threeForTwoDeal.h"
+#include <memory>
 
-threeForTwoDeal::threeForTwoDeal(const std::string& name, int id) : Deal(name, id) {}
+threeForTwoDeal::threeForTwoDeal(const std::string &name, int id)
+    : Deal(name, id) {}
 
-std::vector<Item> threeForTwoDeal::apply (
-    std::vector<std::pair<Item, int>>& itemsMap) {
-    std::vector<Item> discounted;
+std::vector<std::shared_ptr<Item>>
+threeForTwoDeal::apply(std::vector<std::shared_ptr<Item>>& items) {
+    std::vector<std::shared_ptr<Item>> discounted;
+
+    // itemId : (Count, Items[])
+    std::unordered_map<int, std::pair<int, std::vector<std::shared_ptr<Item>>>> itemsMap;
+
+
+    // Add items to HashMap and their quantity
+    for (auto &item : items) {
+        int itemId = item->getId();
+        if (itemsMap.find(itemId) == itemsMap.end()) {
+            itemsMap[itemId] = std::make_pair(1, std::vector<std::shared_ptr<Item>>{item});
+        } else {
+            itemsMap[itemId].first++;
+            itemsMap[itemId].second.push_back(item);
+        }
+    }
+
     for (auto &entry : itemsMap) {
-        auto &item = entry.first;
-        int &itemCount = entry.second;
+        int &itemCount = entry.second.first;
+        auto itemsVector = entry.second.second;
 
-        while (itemCount > 2) {
+        while (itemCount >= 3) {
+            auto item = itemsVector.back();
+            item->setDealApplied(true);
+            itemsVector.pop_back();
+
             discounted.push_back(item);
             itemCount -= 3;
-            }
         }
+    }
 
     return discounted;
 }
