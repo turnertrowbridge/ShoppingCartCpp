30983706f8033a2619ef5639a227ef5bf6c86fe3
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-12 14:51:12 -0700

    Moved logic from cart into apply method

--- threeForTwoDeal.h
+++ threeForTwoDeal.h
@@ -11,7 +11,7 @@ class threeForTwoDeal : public Deal {
 public:
     threeForTwoDeal(const std::string& name, int id);
 
-    std::vector<Item> apply(std::vector<std::pair<Item, int>>& itemsMap) override;
+    std::vector<std::shared_ptr<Item>> apply(std::vector<std::shared_ptr<Item>>& items) override;
 };
 
 #endif
