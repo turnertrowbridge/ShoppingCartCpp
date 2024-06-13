9ba2aa9e82a627542e791b8c40834b5702f8a57e
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-13 10:55:28 -0700

    Removed old includes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 // main.cpp
 
 #include "Checkout.h"
-#include "item.h"
-#include "cart.h"
-#include "threeForTwoDeal.h"
-#include "buyThreeOneFreeDeal.h"
-#include <iomanip>
-
-#include <iostream>
 
 int main() {
     Checkout checkout;
