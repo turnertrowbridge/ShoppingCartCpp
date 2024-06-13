aeeea85004382106cfa53b689b6dd556d5db5256
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-12 17:46:26 -0700

    Updated for checkout class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // main.cpp
 
+#include "Checkout.h"
 #include "item.h"
 #include "cart.h"
 #include "threeForTwoDeal.h"
@@ -9,46 +10,8 @@
 #include <iostream>
 
 int main() {
-    auto threeFor2Deal = std::make_shared<ThreeForTwoDeal>("Three For Two Deal",1);
-    auto oneFreeDeal = std::make_shared<BuyThreeOneFreeDeal>("Buy Three One Free Deal",2);
-
-    Item apple("Apple", 1, 0.60);
-    Item orange("Orange", 2, 0.25);
-    Item banana("Banana", 3, 0.20);
-    Item papaya("Papaya", 4, 0.50);
-    apple.addDeal(threeFor2Deal);
-    orange.addDeal(threeFor2Deal);
-    apple.addDeal(oneFreeDeal);
-    banana.addDeal(oneFreeDeal);
-
-
-    Cart cart;
-    cart.addDeal(threeFor2Deal);
-    cart.addDeal(oneFreeDeal);
-
-    cart.addItem(apple);
-    cart.addItem(apple);
-    cart.addItem(apple);
-    cart.addItem(apple);
-    cart.addItem(apple);
-    cart.addItem(orange);
-    cart.addItem(orange);
-    cart.addItem(apple);
-    cart.addItem(orange);
-    cart.addItem(banana);
-    cart.addItem(papaya);
-
-
-    std::cout << "Cart Items: " << std::endl;
-
-    std::cout << std::fixed << std::setprecision(2);
-
-    for (auto& item : cart.getCart()) {
-        std::cout << item.getName() << " ~ $" << item.getPrice() << std::endl;
-    }
-
-    cart.checkout();
-
+    Checkout checkout;
+    checkout.menu();
 
     return 0;
 }
